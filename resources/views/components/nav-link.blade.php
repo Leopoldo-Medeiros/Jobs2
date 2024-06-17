8f8479a9bb9a41b7020128310d5126bf85772f68
Leopoldo-Medeiros <leopoldof.medeiros@gmail.com>
2024-06-18 00:13:24 +0100

    :bug: WIP: Removing comments

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,10 +1,5 @@
 @props(['active' => false])
 
-{{--<a {{ $attributes->merge(['class' => request()->is('/') ? 'bg-gray-900 text-white rounded-md px-3 py-2 font-medium' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 font-medium',--}}
-{{--'aria-current' => request()->is('/') ? 'page' : 'false']) }}>--}}
-{{--    {{ $slot }}--}}
-{{--</a>--}}
-
 <a class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover: text-white' }} rounded-md px-3 py-2 text-sm  font-medium"
 aria-current="{{ $active ? 'page': 'false' }}"
 {{ $attributes }}>
