55ca3a8b0c24880a1f390a31102cb98ee5e2d0eb
Leopoldo-Medeiros <leopoldof.medeiros@gmail.com>
2024-06-30 00:51:12 +0100

    :construction: Model Factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,4 +9,10 @@ class Job extends Model {
     use HasFactory;
     protected $table = 'job_listing';
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
 }
