8380b215b753e488b127d93caff2bb39c9341598
Leopoldo-Medeiros <leopoldof.medeiros@gmail.com>
2025-03-26 00:22:41 +0000

    feat: WIP - Authorization/Policy

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -1,14 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 class RegisterUserController extends Controller
 {
     public function create()
     {
-        return view(' auth.register');
-    }
-    public function store()
-    {
         return view('auth.register');
     }
+
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'first_name' => 'required|min:2|max:255',
+            'last_name' => 'required|min:2|max:255',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|min:8|max:255|confirmed',
+        ]);
+
+        $user = User::create([
+            'first_name' => $attributes['first_name'],
+            'last_name' => $attributes['last_name'],
+            'email' => $attributes['email'],
+            'password' => Hash::make($attributes['password']),
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/')->with('success', 'Your account has been created!');
+    }
 }
+
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,16 +1,23 @@
 <x-layout>
     <x-slot:heading>
-        Job
+        Job Details
     </x-slot:heading>
-    {{-- Here I am accessing as the array key --}}
-    <h2 class="font-bold text-xl">{{ $job->title }}</h2>
+ 
+        <h2 class="font-bold text-xl">{{ $job->title }}</h2>
 
-    <p>
-        This job pays <b class="text-red-500">{{ $job->salary }}</b> per year.
-    </p>
+        <p>
+            This job pays <b class="text-red-500">{{ $job->salary }}</b> per year.
+        </p>
 
-    <p class="mt-6">
-        {{-- Here I am accessing the job's id property to generate the URL. "{{ $job->id }}"--}}
-        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
-    </p>
+        @can('edit', $job)
+            <p class="mt-6">
+                <button onclick="window.history.back()" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Back</button>
+                <x-button href="{{ secure_url('/jobs/' . $job->id . '/edit') }}">Edit Job</x-button>
+            </p>
+        @else
+            <p class="mt-6">
+                <button onclick="window.history.back()" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Back</button>
+            </p>
+        @endcan
+
 </x-layout>
